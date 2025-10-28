69913ce27a9404e50dff6587050e1fb8005c052b
rockymount114 <rockymount114@gmail.com>
2025-10-28 09:27:43 -0400

    fix cross warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  outputFileTracingRoot: __dirname, // Or use path.join(__dirname)
 };
+
 
 export default nextConfig;
